7e514294f7a31cc9c6d308b608404de15ac03b41
Richard Downer <richard.downer@xero.com>
2014-08-29 15:32:43 +1200

    Started this tutorial

---
+++ AngularJSAuthentication.API/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly : OwinStartup(typeof(AngularJSAuthentication.API.Startup))]
+namespace AngularJSAuthentication.API
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
