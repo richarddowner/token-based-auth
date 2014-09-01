78b26083c78dc9e309c09b8c2c90655c06557ca7
Richard Downer <richard.downer@xero.com>
2014-09-01 14:16:29 +1200

    Seems to be no SimpleAuthorizationServerProvider

--- AngularJSAuthentication.API/Startup.cs
+++ AngularJSAuthentication.API/Startup.cs
@@ -1,5 +1,8 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
 using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
 
 [assembly : OwinStartup(typeof(AngularJSAuthentication.API.Startup))]
@@ -9,9 +12,88 @@ namespace AngularJSAuthentication.API
     {
         public void Configuration(IAppBuilder app)
         {
+            ConfigureOAuth(app);
             HttpConfiguration config = new HttpConfiguration();
             WebApiConfig.Register(config);
             app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthorizationServerOptions = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthorizationServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+
+    public class SimpleAuthorizationServerProvider : IOAuthAuthorizationServerProvider
+    {
+        public Task MatchEndpoint(OAuthMatchEndpointContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ValidateClientRedirectUri(OAuthValidateClientRedirectUriContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ValidateAuthorizeRequest(OAuthValidateAuthorizeRequestContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ValidateTokenRequest(OAuthValidateTokenRequestContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GrantAuthorizationCode(OAuthGrantAuthorizationCodeContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GrantRefreshToken(OAuthGrantRefreshTokenContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GrantClientCredentials(OAuthGrantClientCredentialsContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GrantCustomExtension(OAuthGrantCustomExtensionContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AuthorizeEndpoint(OAuthAuthorizeEndpointContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TokenEndpoint(OAuthTokenEndpointContext context)
+        {
+            throw new NotImplementedException();
         }
     }
 }
